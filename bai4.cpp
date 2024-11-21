48049fc099fa1f4d11bed6e844c4e33ac0c332af
dai611351 <dai611351@gmail.com>
2024-11-21 23:02:37 +0700

    Add files via upload

---
+++ bai4.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    float toan, van, anh, tong, diemtrungbinh;
+
+    printf("Nhap diem Toan: ");
+    scanf("%f", &toan);
+
+    printf("Nhap diem Van: ");
+    scanf("%f", &van);
+
+    printf("Nhap diem Anh: ");
+    scanf("%f", &anh);
+
+    tong = toan + van + anh;
+    diemtrungbinh = tong / 3;
+
+    printf("Tong diem: %.2f\n", tong);
+    printf("Diem trung binh: %.2f\n", diemtrungbinh);
+
+    return 0;
+}
